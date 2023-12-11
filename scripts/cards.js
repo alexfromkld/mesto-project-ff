60d0d866701d97ab8468cd04a63cef31a733e1e7
Алексей Шиян <alexeyshiyan@Aleksejs-MacBook-Pro.local>
2023-12-11 05:16:03 +0200

    done

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Калининград",
+      link: "https://images.unsplash.com/photo-1653467116208-50abc0782a3f?q=80&w=3891&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Ашдод",
+      link: "https://images.unsplash.com/photo-1645036809633-74157165012b?q=80&w=4160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Хайфа",
+      link: "https://images.unsplash.com/photo-1601565530031-80bd2ba63293?q=80&w=4142&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Каунас",
+      link: "https://images.unsplash.com/photo-1632237879328-d9916597a5a3?q=80&w=4000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Бат-ям",
+      link: "https://images.unsplash.com/photo-1601324076323-fd7d61702bc2?q=80&w=3440&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Тель-Авив",
+      link: "https://images.unsplash.com/photo-1552748399-4f659fea71fe?q=80&w=4000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
 
+const cardsTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const cardsList = document.querySelector('.places__list')
 
 // @todo: Функция создания карточки
 
+function makeCard(name, link, deleteCard) {
+  let card = cardsTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = link;
+  card.querySelector('.card__title').textContent = name;
+
+  let deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard)
+
+  return card;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(event) {
+  let cardEl = event.target.closest('.card');
+  cardEl.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(item => cardsList.append(makeCard(item.name, item.link, deleteCard)));
